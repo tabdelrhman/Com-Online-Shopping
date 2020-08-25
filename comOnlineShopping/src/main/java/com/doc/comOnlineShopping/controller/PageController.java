bf53df0a95a0aebc9e903ac74fd907c95fe88d80
abdtahmed <abdtahmed.mail>
2020-08-25 04:02:38 +0300

    Load Static resources..

---
+++ comOnlineShopping/src/main/java/com/doc/comOnlineShopping/controller/PageController.java
@@ -0,0 +1,16 @@
+package com.doc.comOnlineShopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = {"/","/home","/page"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting" , "Welcome to Spring MVC");
+		return mv;
+	}
+}
